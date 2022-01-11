fb6a6ed8d7565fb3f50efd0d3181364beeb8828c
山人自有妙计 <wangdaoo@yeah.net>
2022-01-11 13:59:06 +0800

    feat: wait 等待功能实现 :rocket:

---
+++ basic/wait.ts
@@ -0,0 +1,5 @@
+/** 
+ * wait
+ * @param {number} milliseconds
+ */
+export const wait = async (milliseconds: number) => new Promise(resolve => setTimeout(resolve, milliseconds));
--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { isString } from './basic/isString';
 export { getTypeOf } from './basic/getTypeOf';
+export { wait } from './basic/wait';
