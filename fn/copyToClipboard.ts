60eac983b5baad4b47071ee23d7fb9a2735f8b4d
山人自有妙计 <wangdaoo@yeah.net>
2022-03-05 21:18:43 +0800

    feat: calc average :rocket:

--- fn/copyToClipboard.ts
+++ fn/copyToClipboard.ts
@@ -6,23 +6,23 @@
  */
 export const copyToClipboard = (text: string): Promise<void> => {
   return new Promise((resolve, reject) => {
-    const textArea = document.createElement("textarea");
+    const textArea = document.createElement('textarea');
     textArea.value = text;
-    textArea.style.position = "fixed";
-    textArea.style.top = "0";
-    textArea.style.left = "0";
-    textArea.style.opacity = "0";
+    textArea.style.position = 'fixed';
+    textArea.style.top = '0';
+    textArea.style.left = '0';
+    textArea.style.opacity = '0';
     document.body.appendChild(textArea);
     textArea.focus();
     textArea.select();
     try {
       // execCommand() API 已废弃⚠️
       // document.execCommand("copy");
-      document.queryCommandValue("copy");
+      document.queryCommandValue('copy');
       resolve();
     } catch (err) {
       reject(err);
     }
     document.body.removeChild(textArea);
   });
-}
+};
