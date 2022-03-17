a76f7adf339a0b32dfa280aadea003fc4934d05c
山人自有妙计 <wangdaoo@yeah.net>
2022-03-17 11:33:51 +0800

    feat: npm :tada:

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,32 +1,6 @@
 declare const getTypeOf: (param: unknown) => string;
 
-declare const isArray: (v: any) => boolean;
-
-declare const isAsyncFunction: (v: any) => boolean;
-
-declare const isBoolean: (v: any) => boolean;
-
-declare const isDate: (v: any) => boolean;
-
-declare const isError: (v: any) => boolean;
-
-declare const isFunction: (v: any) => boolean;
-
-declare const isHTMLDocument: (v: any) => boolean;
-
-declare const isNull: (v: any) => boolean;
-
-declare const isNumber: (v: any) => boolean;
-
-declare const isObject: (v: any) => boolean;
-
-declare const isRegExp: (v: any) => boolean;
-
-declare const isString: (v: any) => boolean;
-
-declare const isSymbol: (v: any) => boolean;
-
-declare const isUndefined: (v: any) => boolean;
+declare const is: (value: any, type: string) => boolean;
 
 declare const wait: (milliseconds: number) => Promise<unknown>;
 
@@ -44,4 +18,4 @@ declare const removeHTMLTag: (str: string) => string;
 
 declare const average: (numbers: number[]) => number;
 
-export { average, clearCookie, copyToClipboard, getCookie, getTypeOf, isArray, isAsyncFunction, isBoolean, isBrowser, isDate, isError, isFunction, isHTMLDocument, isNull, isNumber, isObject, isRegExp, isString, isSymbol, isUndefined, removeHTMLTag, throttle, wait };
+export { average, clearCookie, copyToClipboard, getCookie, getTypeOf, is, isBrowser, removeHTMLTag, throttle, wait };
