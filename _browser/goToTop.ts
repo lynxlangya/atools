8d13ef09601baa3281b35c0fa4ade902cd3fe99a
山人自有妙计 <wangdaoo@yeah.net>
2022-04-22 22:24:22 +0800

    feat: create goToTop :rocket:

--- _browser/goToTop.ts
+++ _browser/goToTop.ts
@@ -5,7 +5,7 @@
  * @desc 📝 平滑滚动到顶部
  */
 export const goToTop = (): void => {
-    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    const c = document?.documentElement?.scrollTop ?? document?.body?.scrollTop;
     if (c > 0) {
         window.requestAnimationFrame(goToTop);
         window.scrollTo(0, c - c / 8);
